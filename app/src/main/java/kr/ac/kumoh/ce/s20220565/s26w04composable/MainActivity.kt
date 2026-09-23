6e20ffca39dc2cac9cde4591d1b021eee5f8b729
noname0718 <thckdals3@gmail.com>
2026-09-23 15:10:14 +0900

    🏗️State Hoisting

--- app/src/main/java/kr/ac/kumoh/ce/s20220565/s26w04composable/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20220565/s26w04composable/MainActivity.kt
@@ -31,7 +31,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kr.ac.kumoh.ce.s20220565.s26w04composable.ui.theme.S26W04ComposableTheme
-import kotlin.math.exp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,24 +46,43 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
+    var count1 by remember { mutableIntStateOf(0) }
+    var count2 by remember { mutableIntStateOf(0) }
+
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
         Column(
-            modifier = Modifier.padding(innerPadding),
-        ){
-            Counter()
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            Counter(
+                modifier = Modifier.background(Color(0xFFE8DEF8)),
+                count = count1
+            ) {
+                count1 = it
+            }
+
+            Counter(
+                modifier = Modifier.background(Color(0XFFE9F680)),
+                count = count2
+            ) {
+                count2 = it
+            }
         }
     }
 }
 
 @Composable
-fun ColumnScope.Counter() {
-    var count by remember { mutableIntStateOf((0)) }
+fun ColumnScope.Counter(
+    modifier: Modifier = Modifier,
+    count: Int,
+    onChangeCount: (Int) -> Unit,
+) {
+//    var count by remember { mutableIntStateOf(0) }
     var expanded by remember { mutableStateOf(false) }
+
     Column(
-        modifier = Modifier
+        modifier = modifier
             .weight(1F)
-            .padding(8.dp)
-            .background(Color(0xFFE9F680)),
+            .padding(8.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -78,13 +96,14 @@ fun ColumnScope.Counter() {
             fontSize = 100.sp,
             textAlign = TextAlign.Center,
         )
+
         Row {
             Button(
                 modifier = Modifier
                     .weight(1f)
                     .padding(8.dp),
                 onClick = {
-                    count++
+                    onChangeCount(count + 1)
                 }
             ) {
                 Icon(
@@ -105,28 +124,29 @@ fun ColumnScope.Counter() {
                 )
             }
         }
+
         AnimatedVisibility(expanded) {
-            Row{
+            Row {
                 Button(
                     modifier = Modifier
                         .weight(1f)
                         .padding(8.dp),
                     onClick = {
-                        count --
+                        onChangeCount(count - 1)
                         expanded = false
                     }
-                ){
+                ) {
                     Text("감소")
                 }
                 Button(
                     modifier = Modifier
-                        .weight(1F)
+                        .weight(1f)
                         .padding(8.dp),
                     onClick = {
-                        count = 0
+                        onChangeCount(0)
                         expanded = false
                     }
-                ){
+                ) {
                     Text("초기화")
                 }
             }
